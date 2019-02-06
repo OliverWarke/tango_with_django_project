c374b913dbc94e272e7c90b27ab8e0d3e908b6e0
Oliver Warke <2324909W@student.gla.ac.uk>
2019-02-06 16:19:56 +0000

    Chapter9

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
@@ -29,4 +30,12 @@ class Page(models.Model):
 
 class PageAdmin(admin.ModelAdmin):
     list_display=('title','category','url')
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username
 
